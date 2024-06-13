b7f7c52ee6871105c9181c2ea182881b35df3ec9
oram1288 <cody.oram@keyin.com>
2024-06-13 12:25:18 -0230

    june13

--- Sprint1-lecture/trash.js
+++ Sprint1-lecture/trash.js
@@ -1,22 +1,22 @@
 global.DEBUG = true;
-const http = require("http");
-const fs = require("fs");
-const { parse } = require("querystring");
-const { newToken, tokenCount } = require("./token");
+const http = require('http');
+const fs = require('fs');
+const { parse } = require('querystring');
+const { newToken, tokenCount } = require('./token');
 
 const server = http.createServer(async (req, res) => {
-  let path = "./views/";
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      res.statusCode = 200;
-      fetchFile(path);
-      break;
-    case "/new":
-      if (req.method === "POST") {
-        collectRequestData(req, (result) => {
-          var theToken = newToken(result.username);
-          res.end(`
+    let path = './views/';
+    switch(req.url) {
+        case '/':
+            path += "index.html";
+            res.statusCode = 200;
+            fetchFile(path);
+            break;
+        case '/new':
+            if (req.method === 'POST') {
+                collectRequestData(req, result => {
+                    var theToken = newToken(result.username);
+                    res.end(`
                     <!doctype html>
                     <html>
                     <body>
@@ -25,16 +25,17 @@ const server = http.createServer(async (req, res) => {
                     </body>
                     </html>
                 `);
-        });
-      } else {
-        path += "newtoken.html";
-        res.statusCode = 200;
-        fetchFile(path);
-      }
-      break;
-    case "/count":
-      var theCount = await tokenCount();
-      res.end(`
+                });
+            } 
+            else {
+                path += "newtoken.html";
+                res.statusCode = 200;
+                fetchFile(path);
+            };
+            break;
+        case '/count':
+            var theCount = await tokenCount();
+            res.end(`
                 <!doctype html>
                 <html>
                 <body>
@@ -43,37 +44,38 @@ const server = http.createServer(async (req, res) => {
                 </body>
                 </html>
             `);
-    default:
-      break;
-  }
+        default:
+            break;
+    };
 
-  function fetchFile(path) {
-    fs.readFile(path, function (err, data) {
-      if (err) {
-        console.log(err);
-        res.end();
-      } else {
-        if (DEBUG) console.log("file was served.");
-        res.writeHead(res.statusCode, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-      }
-    });
-  }
+    function fetchFile(path) {
+        fs.readFile(path, function(err, data) {
+            if(err) {
+                console.log(err);
+                res.end();
+            } else {
+                if(DEBUG) console.log('file was served.');
+                res.writeHead(res.statusCode, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            }   
+        })
+    };
 });
 server.listen(3000);
 
 function collectRequestData(request, callback) {
-  const FORM_URLENCODED = "application/x-www-form-urlencoded";
-  if (request.headers["content-type"] === FORM_URLENCODED) {
-    let body = "";
-    request.on("data", (chunk) => {
-      body += chunk.toString();
-    });
-    request.on("end", () => {
-      callback(parse(body));
-    });
-  } else {
-    callback(null);
-  }
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
 }
