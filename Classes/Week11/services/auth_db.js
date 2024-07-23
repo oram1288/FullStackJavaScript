ed6f5f1938b7b7b4ee292ddf29d79961c69a9777
oram1288 <cody.oram@keyin.com>
2024-07-23 20:07:29 -0230

    july23

--- Classes/Week11/services/auth_db.js
+++ Classes/Week11/services/auth_db.js
@@ -1,9 +1,9 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: 'peter',
-  host: 'localhost',
-  database: 'Auth',
-  password: 'royisanerd',
-  port: 5433,
+  user: "postgres",
+  host: "localhost",
+  database: "Auth",
+  password: "leafsfan12",
+  port: 5432,
 });
 module.exports = pool;
